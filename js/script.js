cc3028af35571163bc0b3c779cff59baa77cb8f5
Crusbel <crusbeljl@gmail.com>
2017-02-09 15:11:35 -0500

    Maquetado Inicial

---
+++ js/script.js
@@ -0,0 +1,13 @@
+ $(".button-collapse").sideNav();
+ $(document).ready(function() {
+ 	$('.myslider').camera({
+ 		height: '40%',
+ 		fx: 'mosaicSpiralReverse',
+ 		time: 3000,
+ 		loader: 'bar',
+ 		loaderColor: '#2196F3',
+ 		loaderPadding: 0,
+ 		loaderBgColor: '#d1eff0',
+ 		playPause: false
+ 	});
+ });
