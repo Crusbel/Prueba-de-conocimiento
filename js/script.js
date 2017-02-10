e7925c96f464e036aa201aeb6209673c0ef43289
Crusbel <crusbeljl@gmail.com>
2017-02-10 10:48:08 -0500

    Mejorando estilos
    Mejorando estilos y validando formulario

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
- $(".button-collapse").sideNav();
- $(document).ready(function() {
+$(document).ready(function() {
  	$('.myslider').camera({
  		height: '40%',
  		fx: 'mosaicSpiralReverse',
@@ -8,6 +7,37 @@
  		loaderColor: '#2196F3',
  		loaderPadding: 0,
  		loaderBgColor: '#d1eff0',
- 		playPause: false
- 	});
- });
+ 		playPause: false,
+            mobileNavHover: false
+ 	});  
+});
+
+$(".button-collapse").sideNav();
+
+$('.dropdown-button').dropdown({
+      belowOrigin: true,
+      hover: true,
+      outDuration : 500
+});
+
+$('#myform').validate({
+      rules:{
+            username:{ required: true, minlength: 4 },
+            email: { required: true, email: true },
+            password: { required: true, minlength: 4 },
+            message: { required: true, minlength: 4 }
+      },
+      messages: {
+            username: { required: "Este campo es requerido", minlength: "Debe contener más de 4 caracteres" },
+            email: { required: "Este campo es requerido", email: "No parece un email" },
+            password: { required: "Este campo es requerido", minlength: "Debe contener más de 4 caracteres" },
+            message: { required: "Este campo es requerido", minlength: "Debe contener más de 4 caracteres" }
+      }
+});
+
+$('#myform').on('keyup blur', function() {
+      if ($('#myform').validate().checkForm())
+            $('#btn_submit').removeClass('disabled');
+      else
+            $('#btn_submit').addClass('disabled');
+});
